3dfe2d674f7d01f7c1f57e36060c503b7d49237b
Mehedi Hasan <50661820+mehediH-shakil@users.noreply.github.com>
2023-06-20 15:04:11 +0600

    Delete cart directory

--- cart/models.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import User
-from django.db import models
-from product.models import Product
-
-
-class Cart(models.Model):
-    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(default=1)
