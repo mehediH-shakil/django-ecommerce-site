564e68716629a28f3570fa2c3f361f85ee9d3999
Mehedi Hasan <50661820+mehediH-shakil@users.noreply.github.com>
2023-06-20 15:02:42 +0600

    Delete payment directory

--- payment/views.py
+++
@@ -1,8 +0,0 @@
-from django.urls import reverse
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
-
-
-@login_required
-def checkout(request):
-    return render(request, 'checkout.html')
