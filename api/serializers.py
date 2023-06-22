7d506720041b1db68413a965a6ae6e7053c80a9a
Mehedi Hasan <50661820+mehediH-shakil@users.noreply.github.com>
2023-06-22 17:30:16 +0600

    Update serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
 from product.models import Product
+from cart.models import Cart
 
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
+        fields = '__all__'
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cart
         fields = '__all__'
